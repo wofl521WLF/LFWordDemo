61315ef52d0d6bab9aa0ce59a44df9aa1af3c419
吴林丰 <hanshanshi@7lk.com>
2017-08-09 14:03:13 +0800

    Initial Commit

---
+++ LFWordView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LFWordView
+//
+//  Created by 吴林丰 on 2017/8/9.
+//  Copyright © 2017年 吴林丰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LFWordView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LFWordView
+//
+//  Created by 吴林丰 on 2017/8/9.
+//  Copyright © 2017年 吴林丰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
