c0f7b88f08529fb7e10322b030c745946eb35657
吴林丰 <wulinfeng@chehang168.com>
2019-03-08 09:28:10 +0800

    我测试一把

--- LFWordView/AppDelegate.m
+++ LFWordView/AppDelegate.m
@@ -16,6 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSLog(@"哈哈哈哈哈哈哈");
     // Override point for customization after application launch.
     return YES;
 }
